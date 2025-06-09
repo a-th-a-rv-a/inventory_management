78fdd51fcf26dde9816f77500aedb410a8762cf3
a-th-a-rv-a <atharvasant3@gmail.com>
2025-06-10 00:38:55 +0530

    Update urban_retail_forward_filling.sql

--- urban_retail_forward_filling.sql
+++ urban_retail_forward_filling.sql
@@ -1,3 +1,6 @@
+
+-- CREATING A NEW TABLE FOR FORWARD FILLING
+
 CREATE TABLE corrected_inventory_ff LIKE corrected_inventory;
 
 
@@ -29,8 +32,8 @@ SELECT
   `Category`,
   `Region`,
   @inv := IF(is_new_group OR `Inventory Level` IS NOT NULL, `Inventory Level`, @inv) AS `Inventory Level`,
-  @sold := IF(is_new_group OR `Units Sold` IS NOT NULL, `Units Sold`, @sold) AS `Units Sold`,
-  @ord := IF(is_new_group OR `Units Ordered` IS NOT NULL, `Units Ordered`, @ord) AS `Units Ordered`,
+  @sold := IF(is_new_group OR `Units Sold` IS NOT NULL, `Units Sold`, 0) AS `Units Sold`,
+  @ord := IF(is_new_group OR `Units Ordered` IS NOT NULL, `Units Ordered`, 0) AS `Units Ordered`,
   
   @dem := IF(is_new_group OR `Demand Forecast` IS NOT NULL, `Demand Forecast`, @dem) AS `Demand Forecast`,
   @price := IF(is_new_group OR `Price` IS NOT NULL, `Price`, @price) AS `Price`,
@@ -104,8 +107,96 @@ FROM corrected_inventory_ff;
 
 SELECT *
 FROM corrected_inventory_zeroed
-WHERE `Product ID`='P0057' AND `Store ID`='S001' AND `Region`='East'
+WHERE `Product ID`='P0016' AND `Store ID`='S001' AND `Region`='East'
 ORDER BY `Date`, `Store ID`, `Product ID`;
 
+SELECT `Date`, `Seasonality`
+FROM corrected_inventory_zeroed
+ORDER BY `Date` ASC;
+
+
+-- REPLACING NULL VALUES IN THE SEASONALITY COLUMN OF THE FORWARD-FILLED TABLE WITH ACTUAL SEASONALITY
+
+UPDATE corrected_inventory_zeroed
+SET `Seasonality`='Winter'
+WHERE `Date` BETWEEN '2022-01-01' and '2022-02-28';
+
+
+UPDATE corrected_inventory_zeroed
+SET `Seasonality`='Spring'
+WHERE `Date` BETWEEN '2022-03-01' and '2022-04-30';
+
+UPDATE corrected_inventory_zeroed
+SET `Seasonality`='Summer'
+WHERE `Date` BETWEEN '2022-05-01' and '2022-07-31';
+
+UPDATE corrected_inventory_zeroed
+SET `Seasonality`='Autumn'
+WHERE `Date` BETWEEN '2022-08-01' and '2022-10-31';
+
+UPDATE corrected_inventory_zeroed
+SET `Seasonality`='Winter'
+WHERE `Date` BETWEEN '2022-11-01' and '2023-02-28';
+
+UPDATE corrected_inventory_zeroed
+SET `Seasonality`='Spring'
+WHERE `Date` BETWEEN '2023-03-01' and '2023-04-30';
+
+UPDATE corrected_inventory_zeroed
+SET `Seasonality`='Summer'
+WHERE `Date` BETWEEN '2023-05-01' and '2023-07-31';
+
+UPDATE corrected_inventory_zeroed
+SET `Seasonality`='Autumn'
+WHERE `Date` BETWEEN '2023-08-01' and '2023-10-31';
+
+UPDATE corrected_inventory_zeroed
+SET `Seasonality`='Winter'
+WHERE `Date` BETWEEN '2023-11-01' and '2023-12-31';
+
+
+SELECT *
+FROM corrected_inventory_zeroed
+ORDER BY `Date`, `Store ID`, `Product ID`;
+
+
+-- DOING THE SAME FOR corrected_inventory TABLE
+
+UPDATE corrected_inventory
+SET `Seasonality`='Winter'
+WHERE `Date` BETWEEN '2022-01-01' and '2022-02-28';
+
+
+UPDATE corrected_inventory
+SET `Seasonality`='Spring'
+WHERE `Date` BETWEEN '2022-03-01' and '2022-04-30';
+
+UPDATE corrected_inventory
+SET `Seasonality`='Summer'
+WHERE `Date` BETWEEN '2022-05-01' and '2022-07-31';
+
+UPDATE corrected_inventory
+SET `Seasonality`='Autumn'
+WHERE `Date` BETWEEN '2022-08-01' and '2022-10-31';
+
+UPDATE corrected_inventory
+SET `Seasonality`='Winter'
+WHERE `Date` BETWEEN '2022-11-01' and '2023-02-28';
+
+UPDATE corrected_inventory
+SET `Seasonality`='Spring'
+WHERE `Date` BETWEEN '2023-03-01' and '2023-04-30';
+
+UPDATE corrected_inventory
+SET `Seasonality`='Summer'
+WHERE `Date` BETWEEN '2023-05-01' and '2023-07-31';
+
+UPDATE corrected_inventory
+SET `Seasonality`='Autumn'
+WHERE `Date` BETWEEN '2023-08-01' and '2023-10-31';
+
+UPDATE corrected_inventory
+SET `Seasonality`='Winter'
+WHERE `Date` BETWEEN '2023-11-01' and '2023-12-31';
 
 
